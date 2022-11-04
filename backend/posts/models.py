c7557c9c2d7a539da516f07f3b83d055f62a0a7a
yarvod <yar.vod.ole@gmail.com>
2022-11-04 21:05:49 +0300

    fix Channel fields

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -7,8 +7,9 @@ class Channel(models.Model):
     username = models.CharField(unique=True, verbose_name='Юзернейм', max_length=50)
     name = models.CharField(verbose_name='Название чата', max_length=100)
     link = models.URLField(verbose_name='Ссылка на канал', max_length=100, null=True, blank=True)
-    sender_id = models.CharField(unique=True, verbose_name='Телеграм отправитель id', max_length=50, blank=True)
-    uid = models.CharField(unique=True, verbose_name='Телеграм id', max_length=50, blank=True)
+    sender_id = models.CharField(unique=True, verbose_name='Телеграм отправитель id', max_length=50, blank=True,
+                                 null=True)
+    uid = models.CharField(unique=True, verbose_name='Телеграм id', max_length=50, blank=True, null=True)
 
     class Meta:
         verbose_name = 'Канал'
